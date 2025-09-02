0a4f8d00b9dd4dddcc3161d8341588e6369752af
Goh Shu Jie <sgoh0039@student.monash.edu>
2025-09-02 09:11:15 +0800

    updated ipynb to py

--- main.py
+++ main.py
@@ -1,75 +1,51 @@
 from functools import reduce
+import random as rand
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+
+weights = []
+k = int
+form_url = "https://docs.google.com/forms/d/e/1FAIpQLSdYTZb8v-wVrk5GU2QDCmG-t8EGSAyDHRiLTduYxGlwxmCBcQ/viewform?usp=header"
 
 
-glob_page_qs = {}
-glob_chrome_options = webdriver.ChromeOptions()
-# glob_chrome_options.add_argument('--headless')
-glob_chrome_options.add_experimental_option("detach", True)
-glob_driver = webdriver.Chrome(service=Service(ChromeDriverManager(driver_version="139.0.7258.128").install()), options=glob_chrome_options)
+def prompt_weights():
+    rand.seed(333666999)
+    q_counter = 1
+    try:
+        responses = int(input('Enter number of responses: '))
+    except Exception as e:
+        print(f'Error - {e}')
 
-def find_qs():
+    while True:
+        weight = input(f'Enter weights for question {q_counter}: ')
+        if weight == 'x':
+            break
+        else:
+            try:
+                weight = [int(i) for i in weight.split(' ')]
+                weights.append(rand.choices(range(len(weight)), weight, k=responses))
+                q_counter += 1
+            except Exception as e:
+                print(f'Error - {e}')
+        
+    print(weights)
+
+
+def open():
+    global glob_chrome_options
+    global glob_driver
+    
+    glob_chrome_options = webdriver.ChromeOptions()
+    # glob_chrome_options.add_argument('--headless')
+    glob_chrome_options.add_experimental_option("detach", True)
+    glob_driver = webdriver.Chrome(service=Service(ChromeDriverManager(driver_version="139.0.7258.128").install()), options=glob_chrome_options)
     glob_driver.get(form_url)
-    global glob_page_qs
-    elems = None
-    hasNext = True
-    while hasNext:
-        try:
-            titles = WebDriverWait(glob_driver, 3).until(
-                EC.presence_of_all_elements_located((By.XPATH, '//span[@class="M7eMe"]')))
-            titles_text = reduce(lambda x, element: x + [element.get_attribute('textContent')], titles, [])
-            for title in titles_text:
-                if title not in glob_page_qs:
-                    glob_page_qs[title] = {"id": "", "elems": [], "weights":[]}
-                else:
-                    break
 
-            # question box
-            parent_qs = glob_driver.find_elements(By.XPATH, '//div[@class="Qr7Oae" and not(contains(@class, "pQK2A"))]')
-            for i in range(len(parent_qs)):
-                # radio
-                if len(parent_qs[i].find_elements(By.XPATH, './/div[contains(@class, "AB7Lab")]')) != 0:
-                    elems = parent_qs[i].find_elements(By.XPATH, './/div[contains(@class, "AB7Lab")]')
-                    glob_page_qs[titles_text[i]]['id'] = 'radio'
-                    glob_page_qs[titles_text[i]]['elems'] = elems
-                    continue
-                # checkbox
-                elif len(parent_qs[i].find_elements(By.XPATH, './/div[contains(@class, "uHMk6b")]')) != 0:
-                    elems = parent_qs[i].find_elements(By.XPATH, './/div[contains(@class, "uHMk6b")]')
-                    glob_page_qs[titles_text[i]]['id'] = 'checkbox'
-                    glob_page_qs[titles_text[i]]['elems'] = elems
-                    continue
-                # dropdown
-                elif len(glob_driver.find_elements(By.XPATH, './/span[@class="vRMGwf oJeWuf" and text()="Choose"]')) != 0:
-                    chooses = glob_driver.find_elements(By.XPATH, './/span[@class="vRMGwf oJeWuf" and text()="Choose"]')
-                    chooses[i].click()
-                    time.sleep(0.5)
-                    ddbox = parent_qs[i].find_element(By.XPATH, '//div[@class="OA0qNb ncFHed QXL7Te"]')
-                    elems = ddbox.find_elements(By.XPATH, './/div[@class="MocG8c HZ3kWc mhLiyf OIC90c LMgvRb"]')
-                    glob_page_qs[titles_text[i]]['id'] = 'dropdown'
-                    glob_page_qs[titles_text[i]]['elems'] = elems
-                    choose = ddbox.find_element(By.XPATH, './/div[@class="MocG8c HZ3kWc mhLiyf LMgvRb KKjvXb DEh1R"]')
-                    choose.click()
-                    time.sleep(0.5)
-            
-            next = find_next()
-            if next.get_attribute('textContent').lower() != 'next':
-                hasNext = False
-            else: 
-                next.click()
-            time.sleep(1)
-
-        except Exception as e:
-            print(f'Failed to find title: {e}')
-            glob_driver.quit()
-
-    glob_driver.quit()
 
 def find_next():
     try:
@@ -80,36 +56,49 @@ def find_next():
         print(f'Failed to find next button: {e}')
 
 
-def console():
-    global glob_page_qs
-    for key in glob_page_qs.keys():
-        while True:
+def fill_qs():
+    for iter in range(len(weights[0])):
+        open()
+        hasNext = True
+        total_q_counter = 0
+        while hasNext:
             try:
-                w = [int(i) for i in input(f'{key}\nEnter weights: ').split()]
-                prob_sum = reduce(lambda a, b: a + b, w)
-                num_elems = len(glob_page_qs[key]['elems'])
-                if prob_sum == 10 and len(w) == num_elems:
-                    glob_page_qs[key]['weights'] = [x / 10 for x in w]  # Divide by 10 for NumPy
-                    break
-                else:
-                    print(f'Sum {prob_sum} != 10 or {len(w)} weights != {num_elems} elements')
-            except ValueError:
-                print('Invalid input. Enter integers separated by spaces.')
-                continue
+                parent_qs = glob_driver.find_elements(By.XPATH, '//div[@class="Qr7Oae" and not(contains(@class, "pQK2A"))]')
+                for i in range(len(parent_qs)):
+                    if len(parent_qs[i].find_elements(By.XPATH, './/div[contains(@class, "AB7Lab")]')) != 0:    # radio
+                        elems = parent_qs[i].find_elements(By.XPATH, './/div[contains(@class, "AB7Lab")]')
+                        elems[weights[total_q_counter][iter]].click()
 
+                    elif len(parent_qs[i].find_elements(By.XPATH, './/div[contains(@class, "uHMk6b")]')) != 0:  # checkbox
+                        elems = parent_qs[i].find_elements(By.XPATH, './/div[contains(@class, "uHMk6b")]')
+                        elems[weights[total_q_counter][iter]].click()
 
-def fill():
-    glob_driver.get(form_url)
-    for key in glob_page_qs.keys():
-        pass
+                    elif len(glob_driver.find_elements(By.XPATH, './/span[@class="vRMGwf oJeWuf" and text()="Choose"]')) != 0:  # dropdown
+                        chooses = glob_driver.find_elements(By.XPATH, './/span[@class="vRMGwf oJeWuf" and text()="Choose"]')
+                        chooses[i].click()
+                        time.sleep(0.7)
+                        ddbox = parent_qs[i].find_element(By.XPATH, '//div[@class="OA0qNb ncFHed QXL7Te"]')
+                        elems = ddbox.find_elements(By.XPATH, './/div[@class="MocG8c HZ3kWc mhLiyf OIC90c LMgvRb"]')
+                        elems[weights[total_q_counter][iter]].click()
+                        time.sleep(0.5)
+                    total_q_counter += 1
 
+                next = find_next()
+                if next.get_attribute('textContent').lower() != 'next':
+                    hasNext = False
+                else: 
+                    next.click()
+                time.sleep(1)
+                
+            except Exception as e:
+                print(f'Error - {e}')
+                glob_driver.quit()
+        
+        submit = glob_driver.find_elements(By.CSS_SELECTOR, 'span[class*=NPEfkd]')
+        submit[-2].click()
+        time.sleep(0.5)
+        glob_driver.quit()
+    
 
-def main(form_url):
-    find_qs()
-    console()
-    fill()
-
-
-if __name__ == '__main__':
-    form_url = "https://docs.google.com/forms/d/e/1FAIpQLSdYTZb8v-wVrk5GU2QDCmG-t8EGSAyDHRiLTduYxGlwxmCBcQ/viewform"
-    main(form_url)
+    prompt_weights()
+    fill_qs()
